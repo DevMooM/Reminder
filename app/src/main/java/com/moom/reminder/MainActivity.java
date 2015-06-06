a0c247228b1262b2fb42d179b9a557cf3f055f13
MooM <unnopticoom@gmail.com>
2015-06-06 18:33:17 +0300

    Create MainActivity, class and layout

---
+++ app/src/main/java/com/moom/reminder/MainActivity.java
@@ -0,0 +1,16 @@
+package com.moom.reminder;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by MooM on 06.06.2015.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
